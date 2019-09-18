ee70270551d0352ea690d3f1d8c4d3fe6ff01687
komodovaran <komodovaran.ku@gmail.com>
2019-09-18 12:08:54 +0200

    fix images

--- assets/js/lazyloadEventListener.js
+++ assets/js/lazyloadEventListener.js
@@ -3,9 +3,4 @@ window.addEventListener("load", function (event) {
     let lazy = lazyload(images, {
         "src": "data-original",
     });
-
-    for (var i = 0; i < images.length; i++) {
-        images[i].classList.remove('lazy')
-    }
-
 });
